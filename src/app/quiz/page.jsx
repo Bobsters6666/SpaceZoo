b4b3e5cac41beabcf7e23de0eb1ed2e1345e8467
Jason <jasonabc0626@gmail.com>
2024-08-04 11:01:04 +1200

    good

--- src/app/quiz/page.jsx
+++ src/app/quiz/page.jsx
@@ -1,32 +1,32 @@
 "use client";
 import React, { useState } from "react";
-import { QuizModal } from "@/components/quizModal/quizModal";
-import { QuizModal2 } from "@/components/quizModal/quizModal2";
+import { QuizModal } from "../../components/quizModal/quizModal";
+import { QuizModal2 } from "../../components/quizModal/quizModal2";
 
 export default function App() {
-    const [currentQuestion, setCurrentQuestion] = useState(1);
-    const [totalScore, setTotalScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(1);
+  const [totalScore, setTotalScore] = useState(0);
 
-    const handleNextQuestion = (isCorrect) => {
-      if (isCorrect) {
-          setTotalScore(totalScore + 1);
-      }
-      setCurrentQuestion(currentQuestion + 1);
+  const handleNextQuestion = (isCorrect) => {
+    if (isCorrect) {
+      setTotalScore(totalScore + 1);
+    }
+    setCurrentQuestion(currentQuestion + 1);
   };
 
-    return (
-        <div className='app-container'>
-            {currentQuestion === 1 && (
-                <QuizModal onNextQuestion={handleNextQuestion} />
-            )}
-            {currentQuestion === 2 && (
-                <QuizModal2 onNextQuestion={handleNextQuestion} />
-            )}
-            {currentQuestion > 2 && (
-              <div className='score-container'>
-                <p>Total Score: {totalScore}</p>
-              </div>
-            )}
+  return (
+    <div className="app-container">
+      {currentQuestion === 1 && (
+        <QuizModal onNextQuestion={handleNextQuestion} />
+      )}
+      {currentQuestion === 2 && (
+        <QuizModal2 onNextQuestion={handleNextQuestion} />
+      )}
+      {currentQuestion > 2 && (
+        <div className="score-container">
+          <p>Total Score: {totalScore}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
