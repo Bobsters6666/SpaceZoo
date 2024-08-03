9d6797faa19e430a9ff56c397d2ddeccf9adb011
Kinzi <100537572+Kinzi-c@users.noreply.github.com>
2024-08-04 04:13:36 +1200

    Add NZ background pic and path images

--- src/app/nz/page.jsx
+++ src/app/nz/page.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import styles from "./nz.module.css";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className={styles.container}>
+      <Image
+        src="/nz.jpg"
+        alt="New Zealand"
+        layout="fill"
+        priority
+        className="w-screen h-screen -z-10 absolute"
+      />
+      <img src="/path.png" alt="path" className={styles.path} />
+      <img src="/pin.png" alt="start"  className={styles.start}/>
+      <img src="/shuttle.png" alt="shuttle" className={styles.shuttle}  />
+      <img src="/shuttle.png" alt="shuttle" className={styles.shuttle} />
+      <img src="/shuttle.png" alt="shuttle" className={styles.shuttle}/>
+      <img src="/shuttle.png" alt="shuttle" className={styles.shuttle}/>
+      <img src="/flag.png" alt="finish"  className={styles.finish}/>
+
+    </div>
+  );
 }
