75628ced636ed1610b2caa29ab2dd3743aefd4f5
Nevaeh <xiny7803@gmail.com>
2024-08-04 09:24:10 +1200

    fix bg pic.

--- src/app/profile/layout.jsx
+++ src/app/profile/layout.jsx
@@ -4,7 +4,7 @@ import SideBar from "../../components/profile/SideBar"
 const ProfileLayout = ({ children }) => {
 
   return (
-      <div className=" relative min-h-screen bg-cover bg-ceter " style={{ backgroundImage: "url('/profilebg.jpeg')"}}>
+      <div className="h-screen bg-cover bg-ceter w-screen " style={{ backgroundImage: "url('/profilebg.jpeg')"}}>
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         <div className="relative z-10 text-gray-200 flex flex-row pt-[5rem]">
         <SideBar />
