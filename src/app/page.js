3fcbc9c0ada12da1098171a154ffed81844db0ef
Jason <jasonabc0626@gmail.com>
2024-08-03 16:41:29 +1200

    fixed tailwind error

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Map from "@/components/Map";
+import GoogleMap from "@/components/GoogleMap";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Map />
+      <Image
+        src="/tui.jpg"
+        alt="Next.js Logo"
+        layout="fill"
+        priority
+        className="w-screen h-screen"
+      />
     </main>
   );
 }
--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -3,17 +3,17 @@
 import React from "react";
 import {
   APIProvider,
-  Map as VisMap,
+  Map,
   MapCameraChangedEvent,
 } from "@vis.gl/react-google-maps";
 
-const Map = () => (
+const GooglgMap = () => (
   <APIProvider
     apiKey={"AIzaSyDNDfZ6INfle6ygFAmMkIkHEMFuPMs1E3E"}
     onLoad={() => console.log("Maps API has loaded.")}
   >
     <div className="w-96 h-96 bg-black">
-      <VisMap
+      <Map
         defaultZoom={13}
         defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
         onCameraChanged={(ev) =>
@@ -31,4 +31,4 @@ const Map = () => (
   </APIProvider>
 );
 
-export default Map;
+export default GoogleMap;
