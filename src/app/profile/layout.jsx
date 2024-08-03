c484a7c570b789dfd853d78019dbda02e3603536
Jason <jasonabc0626@gmail.com>
2024-08-03 20:44:31 +1200

    m

--- src/app/profile/layout.jsx
+++ src/app/profile/layout.jsx
@@ -1,13 +1,11 @@
-import SideBar from "@/components/profile/SideBar";
+import SideBar from "../../components/profile/SideBar";
 
 const ProfileLayout = ({ children }) => {
   return (
-
-      <div className="flex flex-row bg-[#BCD8C1] min-h-screen pt-[5rem]">
-        <SideBar />
-        {children}
-      </div>
-
+    <div className="flex flex-row bg-[#BCD8C1] min-h-screen pt-[5rem]">
+      <SideBar />
+      {children}
+    </div>
   );
 };
 export default ProfileLayout;
