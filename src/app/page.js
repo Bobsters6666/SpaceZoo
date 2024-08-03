5756da76852a2d36369b6e871804fd3365ee24f5
Jason <jasonabc0626@gmail.com>
2024-08-03 17:36:00 +1200

    hello

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import GoogleMap from "@/components/GoogleMap";
+import TempMap from "@/components/TempMap";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Image
+    <main className="w-screen h-screen overflow-hidden">
+      {/* <Image
         src="/tui.jpg"
         alt="Next.js Logo"
         layout="fill"
         priority
         className="w-screen h-screen"
-      />
+      /> */}
+      <TempMap />
     </main>
   );
 }
