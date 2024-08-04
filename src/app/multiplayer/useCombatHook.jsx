0517571ba0fd27b83ebc16df4f48eded89fd776d
Chris Valenzuela <themegas100@gmail.com>
2024-08-04 16:15:13 +1200

    chance is 40% now

--- src/app/multiplayer/useCombatHook.jsx
+++ src/app/multiplayer/useCombatHook.jsx
@@ -150,7 +150,7 @@ export function useCombatGame() {
 
   const determineCrashWinnerCoinFlip = useCallback((playerCard, opponentCard, playerIndex, opponentIndex) => {
     // 50/50 chance of either player winning
-    return Math.random() < 0.5 ? "player" : "opponent";
+    return Math.random() < 0.4 ? "player" : "opponent";
   }, []);
 
 
