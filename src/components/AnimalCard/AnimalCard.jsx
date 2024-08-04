d6d146982b49f53d0ba8544c5bd831164dc052b9
Jason <jasonabc0626@gmail.com>
2024-08-04 12:02:45 +1200

    card changes

--- src/components/AnimalCard/AnimalCard.jsx
+++ src/components/AnimalCard/AnimalCard.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import "./AnimalCard.css";
 import animals from "../../data/animals";
+import { FaHandFist, FaHeart } from "react-icons/fa6";
 
 const getRandomAnimal = () => {
   const randomIndex = Math.floor(Math.random() * animals.length);
@@ -12,15 +13,15 @@ const AnimalCard = React.memo(() => {
   const getBorderStyle = (rarity) => {
     switch (rarity) {
       case "Common":
-        return "2px solid #cccccc";
+        return ["3px solid #cccccc", "#cccccc"];
       case "Uncommon":
-        return "2px solid #00ff00";
+        return ["3px solid #00ff00", "#00ff00"];
       case "Rare":
-        return "2px solid #0000ff";
+        return ["3px solid #0000ff", "#0000ff"];
       case "Epic":
-        return "2px solid #800080";
+        return ["3px solid #800080", "#800080"];
       case "Legendary":
-        return "2px solid #f80000";
+        return ["3px solid #f80000", "#f80000"];
     }
   };
 
@@ -30,13 +31,30 @@ const AnimalCard = React.memo(() => {
     <ul className="card-wrapper w-[200px]">
       <li className="card w-[200px]">
         <div className="card-inner">
-          <div className="card-front"
-          style={{ border: getBorderStyle(animal.rarity) }}>
+          <div
+            className="card-front"
+            style={{
+              border: getBorderStyle(animal.rarity)[0],
+            }}
+          >
             <img src={animal.photoUrl} alt={`${animal.name} Image`} />
-            <h3>{animal.name}</h3>
-            <i className="attack-stat">Attack: {animal.stats.attack}</i>
-            <i className="health-stat">Health: {animal.stats.health}</i>
-            <p className="back-rarity">{animal.rarity}</p>
+            <div className="font-semibold">
+              <h3 className="drop-shadow-2xl">{animal.name}</h3>
+              <i className="attack-stat flex items-center gap-2">
+                Attack: {animal.stats.attack}{" "}
+                <FaHandFist size={12} color="white" />
+              </i>
+              <i className="health-stat flex items-center gap-2">
+                Health: {animal.stats.health} <FaHeart size={12} color="red" />
+              </i>
+            </div>
+
+            <p
+              className="back-rarity font-bold drop-shadow-lg text-[18px]"
+              style={{ color: getBorderStyle(animal.rarity)[1] }}
+            >
+              {animal.rarity}
+            </p>
           </div>
           <div
             className="card-back"
